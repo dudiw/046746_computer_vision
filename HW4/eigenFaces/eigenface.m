8fa9c22965b891146a8338ce3c269c3af15e5f69
dudiw <dudiwa@campus.technion.ac.il>
2019-06-10 22:14:46 +0300

    added hw4

---
+++ HW4/eigenFaces/eigenface.m
@@ -0,0 +1,30 @@
+
+% load train set (1.a)
+readYaleFaces;
+
+% A - is the training set matrix where each column is a face image
+% train_face_id - an array with the id of the faces of the training set.
+% image1--image20 are the test set.
+% is_face - is an array with 1 for test images that contain a face
+% faec_id - is an array with the id of the face in the test set, 
+%           0 if no face and -1 if a face not from the train-set.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Your Code Here  %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Subtract mean image (1.b)
+
+
+% Compute eigenvectors and report first 5 eigen-faces (2)
+
+
+% Display and compute the representation error for the training images (3)
+
+
+% Compute the representation error for the test images. Classify the test images and report error rate (4)
+
+
+
+
---
+++ HW4/fisherface.m
@@ -0,0 +1,14 @@
+function [meanvec, basis] = fisherface(face_data, label_train, c)
+% FISHERFACE Creates the Fisher face basis for a set of images
+%
+% face_data - cell array containing the face images
+% label_train - the training labels matching c classes
+% c - number of classes to use
+%
+% meanvec - mean vector of the face images
+% basis - Fisher basis of the face images
+%
+
+
+end
+
